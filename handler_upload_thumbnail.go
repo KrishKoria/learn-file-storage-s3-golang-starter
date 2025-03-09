ebcb98c13acdefa1e491e2cbff90ada4fbd00a3f
Krish Koria <krishkoria2004@gmail.com>
2025-03-09 09:42:09 +0000

    remove thumbnail handling and update upload logic for video thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-
+	"io"
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
+	"encoding/base64"
 )
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
@@ -28,10 +29,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
+	video, err := cfg.db.GetVideo(videoID)
+    if err != nil {
+        respondWithError(w, http.StatusNotFound, "Video not found", err)
+        return
+    }
+
+
+	if video.UserID != userID {
+        respondWithError(w, http.StatusUnauthorized, "Not authorized to upload thumbnail for this video", nil)
+        return
+    }
+
 
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20 
+    err = r.ParseMultipartForm(maxMemory)
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Error parsing form", err)
+        return
+    }
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+    if err != nil {
+        respondWithError(w, http.StatusBadRequest, "Error reading thumbnail file", err)
+        return
+    }
+    defer file.Close()
+
+	imageData, err := io.ReadAll(file)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "Error reading file data", err)
+        return
+    }
+
+	encodedData := base64.StdEncoding.EncodeToString(imageData)
+    
+    mediaType := header.Header.Get("Content-Type")
+    dataURL := fmt.Sprintf("data:%s;base64,%s", mediaType, encodedData)
+    
+    video.ThumbnailURL = &dataURL
+
+
+	err = cfg.db.UpdateVideo(video)
+    if err != nil {
+        respondWithError(w, http.StatusInternalServerError, "Failed to update video metadata", err)
+        return
+    }
+	respondWithJSON(w, http.StatusOK, video)
 }
